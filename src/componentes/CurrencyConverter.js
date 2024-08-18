0638a58aff22aff836dd077b9a07f6468007698d
Eduardo De La Cruz <eduardodelacruzg4@gmail.com>
2024-08-18 14:43:38 -0400

    Update CurrencyConverter.js

--- src/componentes/CurrencyConverter.js
+++ src/componentes/CurrencyConverter.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 
     const CurrencyConverter = () => {
     const [amount, setAmount] = useState(1);
